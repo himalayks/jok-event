5e0b665de06fb7417fd862ad7f27d89cd36befcb
himalayks <92115893+himalayks@users.noreply.github.com>
2021-10-08 12:10:26 +0530

    Add files via upload

---
+++ jokd1q4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    printf("the size of int data type is %d\n",sizeof(int));
+    printf("the size of float data type is %d\n",sizeof(float));
+    printf("the size of char data type is %d\n",sizeof(char));
+    printf("the size of double data type is %d\n",sizeof(double));
+    printf("the size of long data type is %d\n",sizeof(long));
+    printf("the size of short data type is %d\n",sizeof(short));
+    printf("the size of boolean data type is %d\n",sizeof(bool));
+    return 0;
+}
---
+++ jokd1q5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num =22;
+    printf("\n the left shift of 22<<1 is = %d",num<<1);
+    printf("\n the right shift of 22>>1 is = %d",num>>2);
+    return 0;
+}
